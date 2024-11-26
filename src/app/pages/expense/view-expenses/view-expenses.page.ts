7781e786eef844af2a2f69b31e936b47eeca1709
Jay Shah <jays2249@gmail.com>
2024-11-26 23:55:51 +0530

    Fix

--- src/app/pages/expense/view-expenses/view-expenses.page.ts
+++ src/app/pages/expense/view-expenses/view-expenses.page.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController, AlertController } from '@ionic/angular';
 import { DatabaseService } from 'src/app/services/database.service';
-import * as XLSX from 'xlsx';
-import { saveAs } from 'file-saver';
 import { Expense } from '../../../models/expense.model';
 
 @Component({
@@ -45,24 +43,6 @@ export class ViewExpensesPage implements OnInit {
   }
 
   exportToExcel() {
-    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(this.expenses);
 
-    // Create a workbook and add the worksheet
-    const workbook: XLSX.WorkBook = {
-      Sheets: { 'Expenses': worksheet },
-      SheetNames: ['Expenses'],
-    };
-  
-    // Generate a buffer from the workbook
-    const excelBuffer: any = XLSX.write(workbook, {
-      bookType: 'xlsx',
-      type: 'array',
-    });
-  
-    // Create a Blob from the buffer
-    const blob = new Blob([excelBuffer], { type: 'application/octet-stream' });
-  
-    // Use FileSaver to save the file
-    saveAs(blob, 'expenses.xlsx');
   }
 }
